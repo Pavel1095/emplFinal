c26871500389d4e182a3b5bdc23f3f43e5661b29
Pavel1095 <9153220501@mail.ru>
2023-09-19 17:23:27 +0300

    1st commit

---
+++ src/main/java/skypro/employeeBookFinal/EmployeeBookFinalApplication.java
@@ -0,0 +1,13 @@
+package skypro.employeeBookFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookFinalApplication.class, args);
+	}
+
+}
